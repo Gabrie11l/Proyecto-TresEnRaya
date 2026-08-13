601f38bb885823baf870579ace2382b579371155
Jared <joaquito@espol.edu.ec>
2026-08-13 18:51:56 -0500

    If agregafo en buildStateTree, para verificacion.

--- Proyecto/src/main/java/com/mycompany/proyecto/modelo/minimax.java
+++ Proyecto/src/main/java/com/mycompany/proyecto/modelo/minimax.java
@@ -21,16 +21,21 @@ public class minimax {
         this.JugadorSimbolo = JugadorSimbolo;
     }
     public Tree<Tablero> buildStateTree(Tablero actualTablero){
+        
         Node<Tablero> raiz = new Node<>(actualTablero);
+        
         for(Movimiento movDeMaquina : actualTablero.obtenerCasillasVacias()){
             Tablero tableroNivel1 = actualTablero.realizarJugada(movDeMaquina.getFila(), movDeMaquina.getColumna(), MaquinaSimbolo);
             Node<Tablero> nodoNivel1 = new Node<>(tableroNivel1);
             
-            for(Movimiento movDeJugador : tableroNivel1.obtenerCasillasVacias()){
-                Tablero tableroNivel2 = tableroNivel1.realizarJugada(movDeJugador.getFila(), movDeJugador.getColumna(), JugadorSimbolo);
-                tableroNivel2.calcularUtilidad(MaquinaSimbolo, JugadorSimbolo);
-                Node<Tablero> nodoNivel2 = new Node<>(tableroNivel2);
-                nodoNivel1.addChild(nodoNivel2);
+            //Agregado el IF por Jared
+            if (tableroNivel1.verificarGanador() == Simbolo.VACIO && !tableroNivel1.estaLleno()) {
+                for(Movimiento movDeJugador : tableroNivel1.obtenerCasillasVacias()){
+                    Tablero tableroNivel2 = tableroNivel1.realizarJugada(movDeJugador.getFila(), movDeJugador.getColumna(), JugadorSimbolo);
+                    tableroNivel2.calcularUtilidad(MaquinaSimbolo, JugadorSimbolo);
+                    Node<Tablero> nodoNivel2 = new Node<>(tableroNivel2);
+                    nodoNivel1.addChild(nodoNivel2);
+                }
                 
             }
             raiz.addChild(nodoNivel1);
@@ -38,12 +43,15 @@ public class minimax {
         return new Tree<>(raiz);
     }
     public int evaluarArbol(Node<Tablero> raiz){
+        
         if(raiz.isLeaf()){
             Tablero tablero = raiz.getData();
             tablero.calcularUtilidad(MaquinaSimbolo, JugadorSimbolo);
             return tablero.getValorUtilidad();
         }
+        
         int utilidadMinima = Integer.MAX_VALUE;
+        
         for(Node<Tablero> hoja : raiz.getChildren()){
             Tablero tablero = hoja.getData();
             tablero.calcularUtilidad(MaquinaSimbolo, JugadorSimbolo);
@@ -55,12 +63,15 @@ public class minimax {
         return utilidadMinima;
     }
     public Movimiento getMejorMovimiento(Tablero actualTablero){
+        
         Tree<Tablero> arbolDeEstados = this.buildStateTree(actualTablero);
         Node<Tablero> raiz = arbolDeEstados.getRoot();
         List<Node<Tablero>> listaNodosNivel1 = raiz.getChildren();
         List<Movimiento> casillasDisponibles = actualTablero.obtenerCasillasVacias();
         Movimiento mejorMovimiento = null;
+        
         int mejorUtilidad = Integer.MIN_VALUE;
+        
         for(int i = 0; i< listaNodosNivel1.size(); i++){
             int utilidadActual = this.evaluarArbol(listaNodosNivel1.get(i));
             if(utilidadActual> mejorUtilidad){
